8d78f86a85794ef3564bee56785b79f61a48f95a
prince7866 <riaz382@gmail.com>
2021-10-17 18:10:10 +0200

    Person dao should retrieve save person from database 🟢 fake

--- src/main/java/no/kristiania/person/PersonDao.java
+++ src/main/java/no/kristiania/person/PersonDao.java
@@ -3,18 +3,21 @@ package no.kristiania.person;
 import javax.sql.DataSource;
 
 public class PersonDao {
+    private Person person;
+
     public PersonDao(DataSource dataSource) {
         //must take care of dataSource, so it knows how to
         //connect to the database
     }
 
     public void save(Person person) {
-
+            //let's take care of the person that is comming in
+        this.person = person;
     }
 
-    public Person retrieve(long id) {
+    public Person retrieve(Long id) {
         //returns a person for us when receiving an id
         //long with small l because I don't want id to be null
-        return null;
+        return person;
     }
 }
